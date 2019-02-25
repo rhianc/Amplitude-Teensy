9ae6c5306592e38124de31a506ebf1c33e332818
Rhian Chavez <rhian@Rhians-MacBook-Pro.local>
2019-02-24 21:28:01 -0500

    added beat detection

--- spectrumFFT/spectrumFFT.ino
+++ spectrumFFT/spectrumFFT.ino
@@ -8,7 +8,7 @@
 #include <math.h>
 
 #define NUM_LEDS 300 // per strip
-#define BIN_WIDTH 1 // lights with the same frequency assignment
+#define BIN_WIDTH 3 // lights with the same frequency assignment
 
 int state = 0; // used to determine which type of lights we currently want
 
@@ -46,6 +46,7 @@ AudioConnection          patchCord1(adc1, fft);
 //empty array for generating 
 int genFrequencyBinsHorizontal[NUM_BINS];
 float genFrequencyLabelsHorizontal[NUM_BINS];
+float logLevelsEqLong[NUM_BINS];
 
 int genFrequencyHalfBinsHorizontal[HALF_NUM_BINS];
 float genFrequencyHalfLabelsHorizontal[HALF_NUM_BINS];
@@ -60,6 +61,7 @@ int counter = 0;
 
 // Run setup once
 void setup() {
+  Serial.begin(9600);
   pinMode(15, INPUT_PULLUP); // pin 14 used for button read
   // the audio library needs to be given memory to start working
   AudioMemory(12); // this is probably why the longer FFT wasn't working, didn't add more memory
@@ -71,7 +73,7 @@ void setup() {
   //color_spectrum_setup(255,0);
 
   //initialize strip objects
-  FastLED.addLeds<NEOPIXEL, 8>(leds, NUM_LEDS); // using pin 8 // 
+  FastLED.addLeds<NEOPIXEL, 8>(leds, NUM_LEDS); // using pin 8 
   FastLED.addLeds<NEOPIXEL, 10>(leds, NUM_LEDS); // using pin 10
   FastLED.show();
 }
@@ -94,6 +96,23 @@ void loop() {
     }
   }
   else if (state == 1){
+    //check if Audio processing for that sample frame is complete
+    if (fft.available()) {
+        color_spectrum_wrap(true);
+        FastLED.show();
+    }
+    //choose any time based modifiers
+    timer = millis();
+    if(timer-startTimer > 100){
+        moving_color_spectrum_half_wrap();    //modifies color mapping
+        startTimer = millis();
+    }
+  }
+  else if (state == 2){
+    beatDetectorUpdate();
+    FastLED.show();
+  }
+  else if (state == 3){
       merge_half_wrap_boring();
       FastLED.show();
       delay(100);
@@ -105,19 +124,25 @@ void loop() {
 //-----------------------------------------------------------------------
 void checkButtonChange(){
   if (digitalRead(15)==LOW){
-    state = (state+1)%3;
+    state = (state+1)%5;
     if (state == 0){
       writeFrequencyBinsHorizontal();
       color_spectrum_half_wrap_setup();
     }
     else if (state == 1){
+      color_spectrum_setup();
+    }
+    else if (state == 2){
+      color_spectrum_half_wrap_boring(); // change to a single color that changes on the beat
+    }
+    else if (state == 3){
       color_spectrum_half_wrap_boring();
     }
     else{
       fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
       FastLED.show();
     }
-    delay(200);
+    delay(100);
   }
 }
 
@@ -132,6 +157,7 @@ void writeFrequencyBinsHorizontal(){
   int binFreq = 43; // stated on website
   for (int i=0; i < NUM_BINS; i++){ // used when starting from end of strip
     genFrequencyBinsHorizontal[i] = ceil(60./NUM_BINS*0.7964*pow(M_E,0.0583*(i + 1)*(60./NUM_BINS)));
+    //genFrequencyBinsHorizontal[i] = NUM_LEDS/512;
   }
   for (int i=0; i < HALF_NUM_BINS; i++){ // used when starting from center of strip
     genFrequencyHalfBinsHorizontal[i] = ceil(60./HALF_NUM_BINS*0.7964*pow(M_E,0.0583*(i + 1)*(60./HALF_NUM_BINS)));
@@ -139,6 +165,12 @@ void writeFrequencyBinsHorizontal(){
   for (int i=0; i<NUM_BINS;i++){ // has the value of the frequency for each bin
     genFrequencyLabelsHorizontal[i] = genFrequencyBinsHorizontal[i]*binFreq + sum;
     sum = genFrequencyLabelsHorizontal[i];
+    if(genFrequencyLabelsHorizontal[i]<4500){
+      logLevelsEqLong[i] = (float(log10(genFrequencyLabelsHorizontal[i])*0.01+70.))/65.;
+    }
+    else{
+      logLevelsEqLong[i] = (float(-log10(genFrequencyLabelsHorizontal[i])*0.01 + 140.))/65.;
+    }
   }
   sum = 0;
   for (int i=0; i<HALF_NUM_BINS;i++){
@@ -160,13 +192,17 @@ void writeFrequencyBinsHorizontal(){
 //NON REACTIVE----SPECTRUM BUILDERS
 
 //creates full spectrum from red -> magenta that maps from 0->NUM_LEDS
-void color_spectrum_setup(int colorRange, int startColor) {
-  for (int i = 0;i < NUM_LEDS; i++){
-    float number = i * colorRange;
-    float number1 = number / NUM_LEDS;
+void color_spectrum_setup() {
+  for (int i = 0;i < NUM_FLEDS; i++){
+    float number = i * 255;
+    float number1 = number/NUM_FLEDS;
     float number2 = floor(number1);
-    leds[i] = CHSV((number2 + startColor),255,255);
-    hsv_leds[i] = CHSV((number2 + startColor),255,255);
+    fleds[i] = CHSV((number2 + startColor),255,0);
+    fleds[NUM_FLEDS - i - 1] = CHSV((number2 + startColor),255,0);
+  }
+  for (int i = 0; i< NUM_LEDS; i++){
+    leds[NUM_LEDS - i - 1] = fleds[i];
+    hsv_leds[NUM_LEDS - i - 1] = fleds[i];
   }
 }
 
@@ -191,7 +227,6 @@ void color_spectrum_half_wrap(bool useEq){
   unsigned int x, freqBin;
   float level;
   int j_val;
-  //int k_val;
   int j;
   int k;
   int right;
@@ -241,6 +276,51 @@ void color_spectrum_half_wrap(bool useEq){
     }
 }
 
+void color_spectrum_wrap(bool useEq){
+  unsigned int x, freqBin;
+  float level;
+  int j_val;
+  int j;
+  int k;
+  int right;
+  int left;
+  freqBin = 0;
+  for (x=0; x < NUM_BINS; x++) {
+    
+      level = fft.read(freqBin, freqBin + genFrequencyBinsHorizontal[x] - 1);
+       //using equal volume contours to create a liner approximation (lazy fit) and normalizing. took curve for 60Db. labels geerates freq in hz for bin
+      //gradient value (0.00875) was calculated but using rlly aggressive 0.06 to account for bassy speaker, mic,  and room IR.Numbers seem way off though...
+      if(useEq==true){
+        level = level*logLevelsEqLong[x]*255*5; 
+      }
+     
+      int only = (NUM_BINS - x - 1)*BIN_WIDTH;
+      // uncomment to see the spectrum in Arduino's Serial Monitor
+      //Serial.println(level);
+      //uncomment for full spec
+      //level = 255;
+
+      if (level>30) {
+          for(int i=0;i<BIN_WIDTH;i++){
+            color_spectrum_wrap_update(only-i,level);
+          }
+          
+      } else {
+          for(int i=0;i<BIN_WIDTH;i++){
+            j_val = hsv_leds[only].val;
+            if(j_val < 15){
+              color_spectrum_wrap_update(only-i,0);
+            }else{
+              color_spectrum_wrap_update(only-i,j_val * decay);
+            }
+          }
+      }
+      // increment the frequency bin count, so we display
+      // low to higher frequency from left to right
+      freqBin = int(freqBin + genFrequencyBinsHorizontal[x]);
+    }
+}
+
 void color_spectrum_half_wrap_update(int index, float level) {
   if(index >= NUM_LEDS || index < 0){
     ;
@@ -257,6 +337,18 @@ void color_spectrum_half_wrap_update(int index, float level) {
   }
 }
 
+void color_spectrum_wrap_update(int index, float level) {
+  if(index >= NUM_LEDS || index < 0){
+    ;
+  }else{
+    int f_index = index;
+    CHSV fled = fleds[f_index];
+    leds[index] = CHSV(fled.hue,255,level);
+    //leds[10] = CHSV(fled.hue,255,255);
+    hsv_leds[index] = CHSV(fled.hue,255,level);
+  }
+}
+
 void moving_color_spectrum_half_wrap(){
   CHSV fled = fleds[0];
   for(int i=0;i<(NUM_FLEDS-1);i++){
@@ -264,6 +356,8 @@ void moving_color_spectrum_half_wrap(){
   }
   fleds[NUM_FLEDS-1] = fled;
 }
+
+
 
 //----------------------------------------------------------------------
 //---------------------------For NON-Reactive---------------------------
@@ -294,4 +388,64 @@ void merge_half_wrap_boring() {
           leds[NUM_LEDS/2 + i] = fleds[i];
         }
     }
+
+
+//----------------------------------------------------------------------
+//-------------------------For Beat Detection---------------------------
+//----------------------------------------------------------------------
+float getBassPower(int maxBin){
+  float power = 0;
+  for (int i = 1; i < maxBin + 1; i++){
+    power += pow(fft.read(i),2);
+  }
+  return power;
+}
+
+float prevBassPower = 0;
+
+bool beatDetector(){
+  // return true if beat detected
+  float newBassPower = getBassPower(17);
+  //Serial.println(newBassPower-prevBassPower); 
+  if (newBassPower - prevBassPower > 0.04){
+    // beat detected!
+    prevBassPower = newBassPower;
+    return true;
+  }
+  else{
+    prevBassPower = newBassPower;
+    return false;
+  }
+}
+
+int tick = 0;
+int beatTimer = 16;
+
+void beatDetectorUpdate(){
+  // already checked if new FFT available
+  if (beatDetector() && beatTimer > 15){
+    if (tick == 0){
+      fill_solid(leds, NUM_LEDS, CRGB::Red);
+    }
+    else{
+      fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    }
+    tick = (tick + 1) % 2;
+    beatTimer = 0;
+  }
+  beatTimer += 1;
+}
+
+
+//----------------------------------------------------------------------
+//------------------------------For Ripple------------------------------
+//----------------------------------------------------------------------
+
+// need to map frequencies to colors (512 different colors)
+// need to input next round of frequencies at beginning 
+
+void timeEvolve() {
+  
+
+}
 
